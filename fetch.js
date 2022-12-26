22577dc8d78252a168a0d6c5774e70fbbc838b1c
corriolgamer@gmail.com <corriolgamer@gmail.com>
2022-12-26 19:32:55 +0200

    1

---
+++ fetch.js
@@ -0,0 +1,8 @@
+const FetchJSON = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => resolve(data))
+    .catch(error => reject(error))
+  });
+};
